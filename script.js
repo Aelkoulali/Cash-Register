0912ba4ffc36f4734becc309d3de5b4ba7bbbe7b
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-10 15:20:35 +0200

    Add currency variable

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
 const cashInDrawer = document.getElementById("cash-in-drawer");
 
+let currency = [
+  ['PENNY', .01],
+  ['NICKEL', .05],
+  ['DIME', .1],
+  ['QUARTER', .25],
+  ['ONE', 1],
+  ['FIVE', 5],
+  ['TEN', 10],
+  ['TWENTY', 20],
+  ['ONE HUNDRED', 100]
+];
+
 // Add addEventListener to purchaseBtn
 purchaseBtn.addEventListener("click",() => {
   let price = 20;
