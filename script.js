52eb4e8d5c39798647a59f1ffa5d06d8e0b76301
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-12 09:40:33 +0200

    Create getChange function

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ let cid = [
 const cash = document.getElementById("cash");
 const change = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
-const cashInDrawer = document.getElementById("cash-in-drawer");
+
 
 let currency = [
-  ['PENNY', .01],
-  ['NICKEL', .05],
-  ['DIME', .1],
-  ['QUARTER', .25],
+  ['PENNY', 0.01],
+  ['NICKEL', 0.05],
+  ['DIME', 0.1],
+  ['QUARTER', 0.25],
   ['ONE', 1],
   ['FIVE', 5],
   ['TEN', 10],
@@ -31,10 +31,18 @@ let currency = [
 
 // Add addEventListener to purchaseBtn
 purchaseBtn.addEventListener("click",() => {
-  let cashValue = parseFloat(cash.value);
-  let changeDue = cashValue - price;
+  const cashValue = parseFloat(cash.value);
+  const changeDue = cashValue - price;
   if(cashValue < price) {
     alert("Customer does not have enough money to purchase the item");
     return;
   }
+  if (cashValue === price){
+    change.innerText = "No change due - customer paid with exact cash";
+  }
 });
+
+// Create getChange function
+const getChange = (chageDue, cid) => {
+  let totalCid = cid.reduce((sum, [_,amount]) => sum + amount); // [_, amout] = ['', amount]
+};
